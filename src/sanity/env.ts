a3c215280904fde03594b70714003e0181d7a673
albert <skrebzowalbert@gmail.com>
2026-05-06 11:56:31 +0200

    fix sanity api version configuration

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,7 +2,7 @@ export const sanityProjectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
 export const sanityDataset =
   process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 export const sanityApiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2026-05-06";
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-05-01";
 
 export const isSanityConfigured = Boolean(
   sanityProjectId && sanityDataset && sanityApiVersion
